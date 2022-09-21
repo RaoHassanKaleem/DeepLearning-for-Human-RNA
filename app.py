c236c5b33a30cdb4824211a3b6000cdafea54cbe
Hassan Kaleem <60068778+RaoHassanKaleem@users.noreply.github.com>
2022-09-21 22:59:24 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from keras.layers.core import Dense, Dropout
 import numpy as np
 
 icon = Image.open('fav.png')
-st.set_page_config(page_title='ORI-Deep', page_icon = icon)
+st.set_page_config(page_title='LSTM-Deep', page_icon = icon)
 
 def seqValidator(seq):
     for i in range(len(seq)):
@@ -20,39 +20,37 @@ def seqValidator(seq):
 
 def createModel():
     model = Sequential()
-    model.add(LSTM(100, input_shape=(822,1)))
-    model.add(Dropout(0.5))
-    model.add(Dense(512, activation='relu'))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.add(Embedding(max_fatures, embed_dim,input_length = X.shape[1]))
+    model.add(SpatialDropout1D(0.4))
+    model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
+    model.add(Dense(2,activation='softmax'))
+    model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
     model.load_weights("model.h5")
     return model
 
 final_df = pd.DataFrame(columns=['Sequence ID', 'Sequence','Indices','Label'])
 seq = ""
 len_seq = 0
-image = Image.open('WebPic.png')
-st.subheader("""ORI-Deep""")
+image = Image.open('WebPic.jpg')
+st.subheader("""LSTM-Deep""")
 st.image(image, use_column_width=True)
-st.sidebar.subheader(("Input Sequence(s) of Length 300 or greater (FASTA FORMAT ONLY)"))
+st.sidebar.subheader(("Input Sequence(s) of Length 41 or greater (FASTA FORMAT ONLY)"))
 fasta_string  = st.sidebar.text_area("Sequence Input", height=200)
             
 st.subheader("Click the Example Button for Sample Data")
 
 if st.button('Example'):
-    st.info("ORI Sequences")
-    st.code(">Seq1\nTGGAGGTACCTGCTCTCCACGCAGTACAGCATCTGGCTGTGTGGTGGCTGCAGTCCAGTGGGTCTGAGAGGGGTGGAGGGGGAAGGTGGAGAAGGAGAGGAGGGAGGAGGGGAGGGAGGGAAAGGTGAGAGGGGAAGACGAAGGGAAGGAGAAGAGGTGGGGGAGGGGGCAGGGGGGCAGCATTGGCCAGGTAAGGGCGTGTCCACTGTGAGGCACCGTGGGATTCCTGGGCGAGGACTGTGAGACACTGGTGAGCGTGTGTGCAGGGCTCTTTCGGGCTTCCTCTGTTTTTAATGACAT", language="markdown")
-    st.code(">Seq2\nCTGGTGCCCCTTGCTTCCCCCATTCTGGAGCCACATATATACCCTACTCTTCTTCAGAACCTGTCCTCAGTGAAGGTTCCTGACCCTCAGGGATACATCCATGACTAATGGCCTTATTTGTTGCAAGAAAAGAAGTACTCCAATGGAAAGAGATCTTTGAGGCCATCTCCCTGAGTATCCCCATGTTATAGTTGAGTAATTTGAAGGTTGGGAGTCTCAAGCCTGAGAGGAGAATTGCATGTGGTGATTCTGACTACAGACCCTGGAGGCAGACTGCCTGGCTTGGAGTCCTGGCTCATC", language="markdown")
-    st.info("Non-ORI Sequences")
-    st.code(">Seq1\nTGTTGGGCACTGTGCTGGGGCATTTGGATATGATTTTACTTCAAGCCTGGGTAGTAGCAATGATTCCATTTATCGGCACTGGTAATGCGGCCCAGAGAGGTGAGGTGACTTGCCCCAGGCTACACAGCTGCTTTGCCTGACTGCACAGCCCAAGTCTTCCCATCTTCCTTGGCTGCCTCCCTTTGGCCTTTGGCCCCCCACTGAAGCCAGGTTTAGAGCCGCACAGGCCTCTTGCCACCCTGGCGGCTGTCGGGGGAAGCGGTTAGCGGTGATGGACCGTCTAGGACCCTTGGGCCGCCC", language="markdown")
-    st.code(">Seq2\nCTCAGGACCTAGCATTGTCTCTTTCTTTGTTCTTAATAGGTCTCCTCAACCACATTGCAAATGCCATGAAGGCAGGATTGAGTCTATCTTGATCACATGTGTACTCCTCGTTCCTAGAACAGCACCTCTAGGTGCTTAATAAAGCACGCAGTCCAAACACAGTTGTGTTATGAGTGATGGGACAAATGAAGAGCTCCCAGCTTGGAGGATGATGTTGGAGGACATTAGAGGAGGATGGCAAGCACTCAGTCAACCAGCCTATGGGTACAGGGGTGGTTTAAGAAGTCATGGAGGAGGGGC", language="markdown")
+    st.info("Positive Sequences")
+    st.code(r">Seq1\UGCCGAGACCUCAAGGCCAAGGGAAUUUUAUUUGUGGGGAG", language="markdown")
+    st.code(r">Seq2\GGCCUGAAAAACAUGAUCAAGGAGGUGGAUGAGGACUUUGA", language="markdown")
+    st.info("Negative Sequences")
+    st.code(r">Seq1\AAACUCCUUCAUCCAAGUCUGGUUCUUCUCCUCUUGGUCCU", language="markdown")
+    st.code(r">Seq2\AAAUCAGGUGGAAGGACAGAGCACCCUUUCACCGUGGAGGA", language="markdown")
 
 
 if st.sidebar.button("SUBMIT"):
     if(fasta_string==""):
-        st.info("Please input the sequence first. Length should be 300 or greater.")
+        st.info("Please input the sequence first. Length should be 41 or greater.")
     fasta_io = StringIO(fasta_string) 
     records = SeqIO.parse(fasta_io, "fasta") 
     for rec in records:
@@ -60,15 +58,15 @@ if st.sidebar.button("SUBMIT"):
         seq=str(rec.seq)
         if(seqValidator(seq)):
             len_seq = len(seq)
-            if (len_seq < 300):
-                st.info("Please input the sequence again. Length should be 300 or greater. Currently length is " + str(len_seq))
-            elif (len_seq == 300):
-                df_temp = pd.DataFrame([[seq_id, seq,'Complete(1-300)','None']], columns=['Sequence ID', 'Sequence','Indices','Label'] )
+            if (len_seq < 41):
+                st.info("Please input the sequence again. Length should be 41 or greater. Currently length is " + str(len_seq))
+            elif (len_seq == 41):
+                df_temp = pd.DataFrame([[seq_id, seq,'Complete(1-41)','None']], columns=['Sequence ID', 'Sequence','Indices','Label'] )
                 final_df = pd.concat([final_df,df_temp], ignore_index=True)
             else:
-                n_seqs = len_seq - 300
+                n_seqs = len_seq - 41
                 for i in range(n_seqs + 1):
-                    sub_seq = seq[i: i+300]
+                    sub_seq = seq[i: i+41]
                     df_temp = pd.DataFrame([[seq_id, sub_seq,str(i+1)+'-'+str(i+300),'None']], columns=['Sequence ID', 'Sequence','Indices','Label'] )
                     final_df = pd.concat([final_df,df_temp], ignore_index=True)
         else:
@@ -82,9 +80,9 @@ if st.sidebar.button("SUBMIT"):
             score = model.predict(fv_array)
             pred_label = np.round_(score, decimals=0, out=None)
             if(pred_label==1):
-                pred_label="ORI"
+                pred_label="Positive"
             else:
-                pred_label="Non-ORI"
+                pred_label="Negative"
             final_df.iloc[iter, 3] = str(pred_label)
 
     st.dataframe(final_df)
