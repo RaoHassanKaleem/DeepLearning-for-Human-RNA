1f8e19cc9f8a2487251f98c82046849470e7d07c
Hassan Kaleem <60068778+RaoHassanKaleem@users.noreply.github.com>
2022-09-22 18:16:34 +0500

    Add files via upload

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -7,18 +7,12 @@ import math
 import numpy as np
 from Bio import SeqIO
 
-def seqToMat(seq):
+def extractFeature(seq):
     max_fatures = 500
     tokenizer = Tokenizer(num_words=max_fatures, split=' ')
     tokenizer.fit_on_texts(seq.values)
     X = tokenizer.texts_to_sequences(seq.values)
     X = pad_sequences(X)
-    return seq
+    return X
 
-def extractFeature (seq):
-    mainVec = calcFV(seq.lower())
-    encs = replace_seq(seq.lower())
-    for x in encs:
-        mainVec.append(x)
-    fv_array=np.asarray(mainVec).reshape((-1, 1,1))
-    return fv_array
+
