aedee8e2cdbb03248be017c682de8f122e30f987
Hassan Kaleem <60068778+RaoHassanKaleem@users.noreply.github.com>
2022-09-22 22:24:57 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -80,8 +80,7 @@ if st.sidebar.button("SUBMIT"):
             temp_seq =  final_df.iloc[iter, 1]
             fv_array = fe.extractFeature(temp_seq)
             score = np.argmax(model.predict(fv_array), axis=-1)
-            pred_label = np.round_(score, decimals=0, out=None)
-            if(pred_label>1).any():
+            if(pred_label==1):
                 pred_label="Positive"
             else:
                 pred_label="Negative"
